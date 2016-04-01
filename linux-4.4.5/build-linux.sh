8805c5deb1220bf69545cb72eb2538536f62162c
z80 <bashkirov.sergey@gmail.com>
2016-04-01 08:21:51 +0300

    a

---
+++ linux-4.4.5/build-linux.sh
@@ -0,0 +1,6 @@
+#~/bin/sh
+cd ../linux-4.4.5
+ARCH=arm
+CROSS_COMPILE=/home/z80/projects/stm32f429-linux/arm-2010q1/bin/arm-uclinux-
+make O=../out/linux-4.4.5
+
---
+++ scripts/conf.sh
@@ -0,0 +1,14 @@
+#~/bin/sh
+cd ../linux-4.4.5
+ARCH=arm
+CROSS_COMPILE=/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/bin/arm-none-eabi-
+#HOME=$PWD
+#export ARCH
+#export CROSS_COMPILE
+#echo $ARCH
+make menuconfig
+#PATH=$PATH:$PWD/openocd/bin:/$PWD/openocd/share/openocd/scripts:/home/z80/projects/stm32-kernel/linux-4.4.5/openocd/share/openocd/scripts/board
+#openocd --file ./stm32f429discovery.cfg
+#PATH=$PATH:/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/bin
+#arm-none-eabi-gdb-py ./vmlinux -tui
+
