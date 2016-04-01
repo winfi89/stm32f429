e006ae4570d0d9bd6ae7c0193e41b76e4476376f
z80 <bashkirov.sergey@gmail.com>
2016-04-01 09:41:26 +0300

    a

--- busybox-1.22.1/build.sh
+++ busybox-1.22.1/build.sh
@@ -1,11 +1,20 @@
 #~/bin/sh
 
+busybox_src=$PWD
+busybox_build=$busybox_src/../out/busybox
+busybox_romfs=$busybox_src/../out/romfs
+mkdir -p $busybox_build
+mkdir -p $busybox_romfs
+cp    -f $PWD/../scripts/busybox_config $busybox_build/.config
+#exit 0
+
 BASE_PATH=/home/z80/projects/stm32f429-linux/arm-2010q1
 COMP_PATH=$BASE_PATH/bin
 INC_PATH=$BASE_PATH/arm-uclinuxeabi/libc/usr/include
 LIB_PATH=$BASE_PATH/arm-uclinuxeabi/libc/usr/lib
-PATH=$COMP_PATH:$INC_PATH:$LIB_PATH:$BASE_PATH/libc/usr:$PATH
+CROSS_COMPILE=arm-uclinuxeabi-
+export PATH=$COMP_PATH:$INC_PATH:$LIB_PATH:$BASE_PATH/libc/usr:$PATH
 
-make ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1 
-make ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1 install CONFIG_PREFIX=../out/busybox-install
+make -C $busybox_src ARCH=arm CROSS_COMPILE=$CROSS_COMPILE O=$busybox_build oldconfig 
+make -C $busybox_build ARCH=arm CROSS_COMPILE=$CROSS_COMPILE CONFIG_PREFIX=$busybox_romfs install
 
--- busybox-1.22.1/conf.sh
+++ busybox-1.22.1/conf.sh
@@ -1,10 +1,10 @@
 #~/bin/sh
 
 COMP_PATH=/home/z80/projects/stm32f429-linux/arm-2010q1/bin
-PATH=$COMP_PATH:$PATH
+export PATH=$COMP_PATH:$PATH
 
-make $PARS defconfig  ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1
-make $PARS menuconfig ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1
+make $PARS defconfig  ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1 oldconfig
+make $PARS menuconfig ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1 oldconfig
 
 
 
