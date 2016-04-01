36b0da3a92919330b4860719e0fc974645ced1f4
z80 <bashkirov.sergey@gmail.com>
2016-04-01 09:09:36 +0300

    Added scripts. But build doesn't work yet...

--- busybox-1.22.1/build.sh
+++ busybox-1.22.1/build.sh
@@ -1,22 +1,11 @@
 #~/bin/sh
-ARCH=arm
-CROSS_COMPILE=CROSS_COMPILE=/home/z80/projects/stm32f429-linux/arm-2010q1/bin/arm-uclinux-
-PATH=/home/z80/projects/stm32f429-linux/arm-2010q1/arm-uclinuxeabi/libc/usr/include:/home/z80/projects/stm32f429-linux/arm-2010q1/arm-uclinuxeabi/libc/usr/lib:$PATH
 
-echo $PATH
-#exit 0
+BASE_PATH=/home/z80/projects/stm32f429-linux/arm-2010q1
+COMP_PATH=$BASE_PATH/bin
+INC_PATH=$BASE_PATH/arm-uclinuxeabi/libc/usr/include
+LIB_PATH=$BASE_PATH/arm-uclinuxeabi/libc/usr/lib
+PATH=$COMP_PATH:$INC_PATH:$LIB_PATH:$BASE_PATH/libc/usr:$PATH
 
-#HOME=$PWD
-#export ARCH
-#export CROSS_COMPILE
-#echo $ARCH
-
-make
-#make CONFIG_PREFIX=/home/z80/projects/stm32-kernel/busybox-install install
-
-
-#PATH=$PATH:$PWD/openocd/bin:/$PWD/openocd/share/openocd/scripts:/home/z80/projects/stm32-kernel/linux-4.4.5/openocd/share/openocd/scripts/board
-#openocd --file ./stm32f429discovery.cfg
-#PATH=$PATH:/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/bin
-#arm-none-eabi-gdb-py ./vmlinux -tui
+make ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1 
+make ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1 install CONFIG_PREFIX=../out/busybox-install
 
--- busybox-1.22.1/conf.sh
+++ busybox-1.22.1/conf.sh
@@ -1,13 +1,10 @@
 #~/bin/sh
-ARCH=arm
-CROSS_COMPILE=/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/bin/arm-none-eabi-
-#HOME=$PWD
-#export ARCH
-#export CROSS_COMPILE
-#echo $ARCH
-make menuconfig
-#PATH=$PATH:$PWD/openocd/bin:/$PWD/openocd/share/openocd/scripts:/home/z80/projects/stm32-kernel/linux-4.4.5/openocd/share/openocd/scripts/board
-#openocd --file ./stm32f429discovery.cfg
-#PATH=$PATH:/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/bin
-#arm-none-eabi-gdb-py ./vmlinux -tui
+
+COMP_PATH=/home/z80/projects/stm32f429-linux/arm-2010q1/bin
+PATH=$COMP_PATH:$PATH
+
+make $PARS defconfig  ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1
+make $PARS menuconfig ARCH=arm CROSS_COMPILE=$COMP_PATH/arm-uclinuxeabi- O=../out/busybox-1.22.1
+
+
 
