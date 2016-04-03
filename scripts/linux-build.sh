a18cbff852005b5c9fcb56c8e4035c0f0e5a041e
z80 <bashkirov.sergey@gmail.com>
2016-04-03 09:51:54 +0300

    a

--- scripts/linux-build.sh
+++ scripts/linux-build.sh
@@ -1,6 +1,5 @@
 #~/bin/sh
 cd ../linux-4.4.5
 ARCH=arm
-CROSS_COMPILE=/home/z80/projects/stm32f429-linux/arm-2010q1/bin/arm-uclinuxeabi-
+CROSS_COMPILE=/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/bin/arm-none-eabi-
 make O=../out/linux-4.4.5
-
--- scripts/linux-debug.sh
+++ scripts/linux-debug.sh
@@ -1,7 +1,6 @@
 #~/bin/sh
 ARCH=arm
-CROSS_COMPILE=/home/z80/projects/stm32f429-linux/arm-2010q1/bin/arm-uclinuxeabi-
+CROSS_COMPILE=/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/bin/arm-none-eabi-
 
-PATH=$PATH:/home/z80/projects/stm32f429-linux/arm-2010q1/bin
-arm-uclinuxeabi-gdb ../out/linux-4.4.5/vmlinux -tui
-
+PATH=$PATH:/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/bin
+arm-none-eabi-gdb ../out/linux-4.4.5/vmlinux -tui
