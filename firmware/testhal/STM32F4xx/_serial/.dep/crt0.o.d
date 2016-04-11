e380aacc67e01d8f76500ba58c60d49e3b2fd7ef
z80 <bashkirov.sergey@gmail.com>
2016-04-11 08:07:45 +0300

    Added chibios test with gyroscope reading.

---
+++ firmware/testhal/STM32F4xx/_serial/.dep/crt0.o.d
@@ -0,0 +1,19 @@
+build/obj/crt0.o: ../../../os/ports/GCC/ARMCMx/crt0.c \
+ /home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/lib/gcc/arm-none-eabi/5.2.1/include/stdint.h \
+ /home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/stdint.h \
+ /home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/machine/_default_types.h \
+ /home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/sys/features.h \
+ /home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/sys/_intsup.h \
+ /home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/sys/_stdint.h
+
+/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/lib/gcc/arm-none-eabi/5.2.1/include/stdint.h:
+
+/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/stdint.h:
+
+/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/machine/_default_types.h:
+
+/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/sys/features.h:
+
+/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/sys/_intsup.h:
+
+/home/z80/programs/sw4stm32/plugins/fr.ac6.mcu.externaltools.arm-none.linux32_1.7.0.201602151102/tools/compiler/arm-none-eabi/include/sys/_stdint.h:
